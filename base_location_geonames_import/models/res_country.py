1c4403f0178abda5f1a0e07098234ab36cfc4a7e
StephaneMangin <stephane.mangin@orinoko.fr>
2021-02-21 17:01:33 +0100

    Add borough import to base_location_geonames_import

--- base_location_geonames_import/models/res_country.py
+++ base_location_geonames_import/models/res_country.py
@@ -1,4 +1,5 @@
 # Copyright 2017 Franco Tampieri, Freelancer http://franco.tampieri.info
+# Copyright 2021 Stéphane Mangin <stephane.mangin@freesbee.fr>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
@@ -10,3 +11,5 @@ class ResCountry(models.Model):
 
     geonames_state_name_column = fields.Integer("Geonames State Name Column")
     geonames_state_code_column = fields.Integer("Geonames State Code Column")
+    geonames_borough_name_column = fields.Integer("Geonames Borough Name Column")
+    geonames_borough_code_column = fields.Integer("Geonames Borough Code Column")
